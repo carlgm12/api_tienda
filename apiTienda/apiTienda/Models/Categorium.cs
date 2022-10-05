889d306e0e85c2b0d53ceb5d89e9b70ea3db700a
Carlos Gonzalez <carlgm12@gmail.com>
2022-10-05 12:16:18 -0300

    primera version

---
+++ apiTienda/apiTienda/Models/Categorium.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace apiTienda.Models
+{
+    public partial class Categorium
+    {
+        public long IdCategoria { get; set; }
+        public string? Descripcion { get; set; }
+    }
+}
